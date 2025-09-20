d1f97ea708085ceb0e328592f6af66431d36367b
ibrahim <i.aliyu019@gmail.com>
2025-09-20 13:13:25 +0100

    feat: update AccordionTrigger styles and adjust border radius in Experience component

--- components/animate-ui/components/base/accordion.tsx
+++ components/animate-ui/components/base/accordion.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { ChevronDownIcon, PlusIcon, MinusIcon } from "lucide-react";
+import { PlusIcon, MinusIcon } from "lucide-react";
 
 import {
   Accordion as AccordionPrimitive,
@@ -45,7 +45,7 @@ function AccordionTrigger({
     <AccordionHeaderPrimitive className="flex">
       <AccordionTriggerPrimitive
         className={cn(
-          "group focus-visible:border-ring focus-visible:ring-ring/50 flex flex-1 items-start justify-between gap-4 rounded-md py-4 text-left text-sm font-medium transition-all outline-none hover:underline focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50",
+          "group focus-visible:border-ring focus-visible:ring-ring/50 flex flex-1 items-center justify-between gap-4 rounded-md py-4 text-left text-sm font-medium transition-all outline-none hover:underline focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50",
           className
         )}
         {...props}
@@ -57,7 +57,7 @@ function AccordionTrigger({
             <PlusIcon
               aria-hidden
               className={cn(
-                "absolute inset-0 m-auto size-4 transition-all duration-200 ease-in-out",
+                "bg-white/5 border border-white/10 rounded-full p-1 absolute inset-0 m-auto size-6 mr-1 transition-all duration-200 ease-in-out",
                 "group-data-[state=open]:opacity-0 group-data-[state=open]:scale-75 group-data-[state=open]:-translate-y-0.5",
                 "group-data-[panel-open]:opacity-0 group-data-[panel-open]:scale-75 group-data-[panel-open]:-translate-y-0.5"
               )}
@@ -67,7 +67,7 @@ function AccordionTrigger({
             <MinusIcon
               aria-hidden
               className={cn(
-                "absolute inset-0 m-auto size-4 opacity-0 scale-75 transition-all duration-200 ease-in-out",
+                " bg-white/5 border border-white/10 rounded-full p-1 absolute inset-0 m-auto size-6 mr-1 opacity-0 scale-75 transition-all duration-200 ease-in-out",
                 "group-data-[state=open]:opacity-100 group-data-[state=open]:scale-100 group-data-[state=open]:translate-y-0",
                 "group-data-[panel-open]:opacity-100 group-data-[panel-open]:scale-100 group-data-[panel-open]:translate-y-0"
               )}
